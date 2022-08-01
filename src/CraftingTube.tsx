57abedbf13cde2addad082677014690daccca5a0
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2022-08-01 19:22:24 +0300

    svg as template

--- src/CraftingTube.tsx
+++ src/CraftingTube.tsx
@@ -2,63 +2,59 @@ import { CraftingAction, SubstanceId } from "./crafting";
 import { substanceColors } from "./substanceColors";
 import * as flex from "./utils/flex";
 import { JSX } from "preact";
-import { useEffect, useState } from "preact/hooks";
+import { useEffect, useState, useRef } from "preact/hooks";
 import { css, cx, keyframes } from "@emotion/css";
 import { buttonCss } from "./buttonCss";
 import { ArrowLeft } from "@emotion-icons/material-rounded/ArrowLeft";
 import { useRecoilValue } from "recoil";
 import { appliedCraftingActionsRecoil, craftingActionsRecoil, tubesState } from "./CraftingTable";
 import { useUpdRecoilState } from "./utils/useUpdRecoilState";
+import { ReactComponent as CraftingTubeSvg } from "./craftingTube.svg";
 
-function Wave({ isFirst, ...props }: { isFirst: boolean, className?: string }) {
-    return <svg
-        id="Layer_2"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 64.87 45.06"
-        {...props}
-    >
-        <g id="Layer_6">
-            <g class="cls-2">
-                {isFirst
-                    ? <path class="cls-1" d="M0,13.42c.83,16.28,13.67,29.37,29.83,30.64h5.12c16.04-1.25,28.81-14.18,29.8-30.29-.84-4.49-9.53,18.63-31.72-3.69C18.09-4.97,.9-2.6,0,13.42Z" />
-                    : <path class="cls-1" d="M33.08,10.08C17.72-5.38,0-2.46,0,14.76v13.77c0,8.57,6.95,15.53,15.53,15.53H49.34c8.58,0,15.53-6.95,15.53-15.53V14.76c0-8.15-8.39,18.86-31.79-4.68Z" />
-                }
-            </g>
-        </g>
-    </svg>;
-}
-
-
-function Slot({ i }: { i: number; }) {
+const PourFromMainIntoSecondaryButton = () => {
     const updCraftingActions = useUpdRecoilState(craftingActionsRecoil);
     const act = (action: CraftingAction) => updCraftingActions({ $push: [action] });
 
+    return <button
+        className={cx(buttonCss)}
+        style={{
+            display: "flex",
+            position: "absolute",
+            left: "-24px",
+            top: ["-10px", "3px", "16px"][i],
+
+            alignItems: "center",
+            width: "23px",
+            height: "40px",
+        }}
+        onClick={() => act({ action: "pourFromMainIntoSecondary", time: performance.now() })}
+    ><ArrowLeft style={{ height: 80, margin: -20 }} /></button>;
+};
+
+function Slot({ i }: { i: number; }) {
     const tubes = useRecoilValue(tubesState);
     const tube = tubes[0];
     const isSecondaryAvailable = tubes.length > 1;
 
-    const isNext = i === tube.length;
     const isTopContent = i === tube.length - 1;
     const hasContent = i < tube.length;
     const isFirst = i === 0;
+
 
     return <div style={{
         textAlign: "center",
         margin: `0px 6px 6px 6px`,
         width: `28px`,
         height: `56px`,
-        border: "2px dashed #ffffff60",
+        border: "2px solid transparent",
         borderRadius: "5px",
         fontSize: "38px",
         lineHeight: "58px",
-        color: "#ffffff60",
-        backgroundColor: "#ffffff08",
+        color: "#ffffffff",
         position: "relative",
 
         ...(hasContent && {
             backgroundColor: substanceColors[tube[i]],
-            color: "#ffffffff",
-            borderColor: "transparent",
         }),
 
         ...(isFirst && {
@@ -66,40 +62,10 @@ function Slot({ i }: { i: number; }) {
             borderBottomRightRadius: "15px",
         }),
     }}>
-        {isNext
-            ? <>
-                <Wave
-                    {...{ isFirst }}
-                    className={css`
-                        & {
-                            position: absolute;
-                            bottom: 0px;
-                            left: 0px;
-                            transform: scale(1.15);
-                        }
-                        & .cls-1 {
-                            fill: transparent;
-                            stroke: #C0C7CF;
-                            stroke-width: 5px;
-                        }
-                    `}
-                />+
-            </>
-            : tube[i]
-        }
-        {isTopContent && isSecondaryAvailable && <button
-            className={cx(buttonCss)}
-            style={{
-                display: "flex",
-                alignItems: "center",
-                position: "absolute",
-                left: "-24px",
-                top: ["-10px", "3px", "16px"][i],
-                width: "23px",
-                height: "40px",
-            }}
-            onClick={() => act({ action: "pourFromMainIntoSecondary", time: performance.now() })}
-        ><ArrowLeft style={{ height: 80, margin: -20 }} /></button>}
+        {tube[i]}
+        {isTopContent &&
+            isSecondaryAvailable &&
+            <PourFromMainIntoSecondaryButton />}
     </div>;
 }
 
@@ -108,26 +74,55 @@ export function CraftingTube({ style }: {
 }) {
     const appliedCraftingActions = useRecoilValue(appliedCraftingActionsRecoil);
 
-    const [time, setTime] = useState<number>();
-    useEffect(() => setTime(performance.now()));
-    if (time === undefined) { return null; }
+    const [time, setTime] = useState(0);
+    useEffect(() => setTime(performance.now()), [appliedCraftingActions]);
 
-    return <div style={{
-        ...flex.colRev,
+    const tube = appliedCraftingActions.stateFinal.tubes[0];
+    const isNext = (i: number) => tube.length === i;
 
-        height: "220px",
-        background: "#ffffff4d",
-        borderRadius: "0px 0px 999px 999px",
-        border: "6px solid #ffffff90",
-        borderTopColor: "#ffffff30",
-        ...style,
-    }}>
-        {("action" in appliedCraftingActions) && (() => {
-            const { action } = appliedCraftingActions;
-            if (action.action !== "addIngredient") { return null; }
-            return <div style={{ position: "relative" }}>
-                <div
-                    className={css`& {
+    return <div className={cx(css`& {
+        width: 57px;
+        position: relative;
+    }`)} style={style}>
+        <CraftingTubeSvg
+            className={cx(css`
+                ${[0, 1, 2].map(i => `
+                    & #slot${i}_add {
+                        ${isNext(i) ? "" : "display: none;"}
+                    }
+                    & #slot${i}_content {
+                        ${tube.length > i ? "" : "display: none;"}
+                    }
+                    & #slot${i}_content_back {
+                        fill: ${substanceColors[tube[i]]};
+                    }
+                    & #slot${i}_number {
+                        font-family: 'Bahnschrift', sans-serif;
+                        text-anchor: middle;
+                    }
+                `).join('\n')}
+            `)}
+            slots={{
+                slot0_number: tube[0],
+                slot1_number: tube[1],
+                slot2_number: tube[2],
+            }}
+        />
+
+        {/* <div style={{
+            ...flex.colRev,
+
+            height: "220px",
+            border: "6px solid transparent",
+            position: "absolute",
+            top: 0,
+        }}>
+            {("action" in appliedCraftingActions) && (() => {
+                const { action } = appliedCraftingActions;
+                if (action.action !== "addIngredient") { return null; }
+                return <div style={{ position: "relative" }}>
+                    <div
+                        className={css`& {
                         position: absolute;
                         animation-name: ${keyframes`
                             0% {
@@ -142,13 +137,14 @@ export function CraftingTube({ style }: {
                         animation-delay: ${action.time - time}ms;
                         animation-fill-mode: both;
                     }`}
-                >
-                    {action.ingredientId}
+                    >
+                        {action.ingredientId}
+                    </div>
                 </div>
-            </div>
-        })()}
-        <Slot i={0} />
-        <Slot i={1} />
-        <Slot i={2} />
+            })()}
+            <Slot i={0} />
+            <Slot i={1} />
+            <Slot i={2} />
+        </div> */}
     </div>;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,63 @@ import { defineConfig } from "vite";
 import { viteSingleFile } from "vite-plugin-singlefile";
 import BuildInfo from 'vite-plugin-info';
 import preact from "@preact/preset-vite";
+import svgr from 'vite-plugin-svgr';
+
+function* traverseJSXElementTree(el) {
+    for (const childEl of el.children) {
+        if (childEl.type === 'JSXElement') {
+            yield* traverseJSXElementTree(childEl);
+        }
+    }
+    yield el;
+};
+const injectSlots = (jsx, tpl) => {
+    for (const el of traverseJSXElementTree(jsx)) {
+        const id = el.openingElement.attributes.find(attr => attr.name.name === "id")?.value.value;
+        if (!id) { return; }
+
+        if (el.openingElement.selfClosing) {
+            el.openingElement.selfClosing = false;
+            el.closingElement = {
+                type: "JSXClosingElement",
+                name: {
+                    type: "JSXIdentifier",
+                    name: el.openingElement.name.name,
+                }
+            }
+        }
+        el.children = el.children ?? [];
+
+        const slotExpr = tpl`<>{props.slots?.${id}}</>`.expression;
+        el.children.push(...slotExpr.children);
+    }
+}
+
+const svgrTemplate = (variables, { tpl }) => {
+    injectSlots(variables.jsx, tpl);
+    return tpl`
+        ${variables.imports};
+        
+        ${variables.interfaces};
+        
+        const ${variables.componentName} = (${variables.props}) => (
+        ${variables.jsx}
+        );
+        
+        ${variables.exports};
+    `;
+}
 
 export default defineConfig({
     plugins: [
         preact({ devtoolsInProd: true }),
         viteSingleFile(),
         BuildInfo(),
+        svgr({
+            svgrOptions: {
+                template: svgrTemplate,
+            }
+        }),
     ],
     server: {
         port: 3685,
