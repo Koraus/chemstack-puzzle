75f15586cb466a3899b3e48570b2edcfdb01b60b
Andriy Kashchynets <andriy.kashchynets@gmail.com>
2022-10-03 00:35:17 +0300

    compacted buildTime in version

--- src/appVersion.ts
+++ src/appVersion.ts
@@ -1,7 +1,37 @@
 import buildTime from '~build/time';
 import { abbreviatedSha as buildGitRevSha } from '~build/info';
 
-const biuldTimeStr = buildTime.toISOString().replaceAll(/[^0-9ZT]/g, "");
+const biuldTimeStr = ((date: Date) => {
+    const digits = "0123456789abcdefghijklmnopqrstuvwxyz";
+    
+    const relYearStr = (() => {
+        // returns 0..v if year relative to anchor year is in renderable range
+        //     OR 00[m]yyyyz (e.g. 1999 -> 001999z) otherwise (this case is hardly applicable for this specific need)
+        const anchorYear = 2022;
+        const year = date.getUTCFullYear();
+        const relYear = year - anchorYear;
+        if (relYear < 0 || relYear >= digits.length) {
+            const _0 = digits[0];
+            const _z = digits[digits.length - 1];
+            const mYearStr = year.toString().replace("-", "m");
+
+            return _0 + _0 + mYearStr + _z;
+        }
+        return digits[relYear];
+    })();
+
+    const monthStr = digits[date.getUTCMonth() + 1]; // Jan is 1, Dec is c=12
+    const dayStr = digits[date.getUTCDate()]; // 1, 2, ..., 9, a, b, ..., v=31
+
+    const startOfDay = new Date(date.toISOString().split("T")[0]);
+    const millisecondOfDay = date.getTime() - startOfDay.getTime();
+    const millisecondsPerDay = 24 * 60 * 60 * 1000;
+    const fractionOfDay = millisecondOfDay / millisecondsPerDay;
+    const fractionOfDayStr = fractionOfDay.toString().substring(2, 6); // 4 digits of precision give resolution of ~9 sec
+
+    return relYearStr + monthStr + dayStr + fractionOfDayStr;
+})(buildTime);
+
 export const appVersion = `0.3.0-rc.1+${biuldTimeStr}-${buildGitRevSha}`
     + (import.meta.env.PROD ? "" : ("-" + import.meta.env.MODE));
 console.log("appVersion", appVersion);
